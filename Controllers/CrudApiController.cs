a581d5c16e896a1fd91ba70ea465025156b504e3
Abrar <abrarkhalifa63@gmail.com>
2023-02-27 20:36:41 +0530

    Add project files.

---
+++ Controllers/CrudApiController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiCrud.Models;
+
+namespace WebApiCrud.Controllers
+{
+    public class CrudApiController : ApiController
+    {
+        DB_WEB_API_CRUDEntities db = new DB_WEB_API_CRUDEntities();
+
+        [HttpGet]
+        public IHttpActionResult GetAllEmployees()
+        {
+            List<Employee> list = db.Employees.ToList();
+            return Ok(list);
+        }
+        //public List<Employee> GetAllEmployees()         --- both will work
+        //{
+
+        //    return db.Employees.ToList();
+        //}
+
+        // this is for getting one result
+        [HttpGet]
+        public IHttpActionResult getone(int id)
+        {
+            var emp = db.Employees.Where(x => x.id == id).SingleOrDefault();
+            return Ok(emp);
+        }
+
+        // this is for insert
+        [HttpPost]
+        public IHttpActionResult EmpInsert(Employee e)
+        {
+            db.Employees.Add(e);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        // this is for update
+        [HttpPut]
+        public IHttpActionResult EmpUpdate(Employee e)
+        {
+            db.Entry(e).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return Ok();
+        }
+
+        // delete 
+        [HttpDelete]
+        public IHttpActionResult DeleteOne(int id)
+        {
+            var emp = db.Employees.Find(id);
+            db.Employees.Remove(emp);
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/CrudMvcController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApiCrud.Models;
+
+namespace WebApiCrud.Controllers
+{
+    public class CrudMvcController : Controller
+    {
+        // GET: CrudMvc
+
+        HttpClient client = new HttpClient();
+        public ActionResult GetEmp()
+        {
+            List<Employee> emp_list = new List<Employee>();
+            client.BaseAddress = new Uri("https://localhost:44328/api/CrudApi");
+            var response = client.GetAsync("CrudApi");
+            response.Wait();
+
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Employee>>();
+                display.Wait();
+                emp_list = display.Result;
+
+            }
+            return View(emp_list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            client.BaseAddress = new Uri("https://localhost:44328/api/CrudApi");
+            var response = client.PostAsJsonAsync<Employee>("CrudApi",emp);
+            response.Wait();
+
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("GetEmp");
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44328/api/CrudApi");
+            var response = client.GetAsync("CrudApi?id="+id.ToString());
+            response.Wait();
+
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                e = display.Result;
+
+            }
+            return View(e);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44328/api/CrudApi");
+            var response = client.GetAsync("CrudApi?id=" + id.ToString());
+            response.Wait();
+
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                e = display.Result;
+
+            }
+            return View(e);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee e)
+        {
+            client.BaseAddress = new Uri("https://localhost:44328/api/CrudApi");
+            var response = client.PutAsJsonAsync<Employee>("CrudApi", e);
+            response.Wait();
+
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("GetEmp");
+
+            }
+            return View("Edit");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44328/api/CrudApi");
+            var response = client.GetAsync("CrudApi?id=" + id.ToString());
+            response.Wait();
+
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                e = display.Result;
+
+            }
+            return View(e);
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            client.BaseAddress = new Uri("https://localhost:44328/api/CrudApi");
+            var response = client.DeleteAsync("CrudApi/"+id.ToString());
+         
+            response.Wait();
+
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("GetEmp");
+
+            }
+            return View("Delete");
+        }
+    }
+}
